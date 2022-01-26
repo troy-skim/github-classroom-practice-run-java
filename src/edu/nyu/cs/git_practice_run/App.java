b9aae55bc63f2afdfadc5c3714cb98928d6d9239
bloombar <amos@cs.nyu.edu>
2022-01-26 15:31:38 -0500

    initial commit

---
+++ src/edu/nyu/cs/git_practice_run/App.java
@@ -0,0 +1,44 @@
+package edu.nyu.cs.git_practice_run;
+
+public class App {
+
+  public static String foo(String x, String y) {
+    /**
+     * Concatenates two strings together and returns the result.
+     * 
+     * @param x a String
+     * @param y another String
+     * @returns The concatenation of x and y
+     */
+
+    return x + " " + y;
+  }
+
+  public static String bar() {
+    /**
+     * @returns The String, "Hello world!"
+     */
+    
+    return "Hello world!";
+  }
+
+  public static void baz() {
+    /**
+     * Prints out the String, "Hello world!"
+     */
+
+    System.out.println("Hello world!");
+  }
+
+  public static void main(String[] args) throws Exception {
+    /**
+     * The main function is automatically called first in a Java program.
+     * You will complete this function.
+     * 
+     * @param args An array of any command-line arguments.
+     */
+
+
+  }
+
+}
---
+++ src/edu/nyu/cs/git_practice_run/AppTest.java
@@ -0,0 +1,56 @@
+package edu.nyu.cs.git_practice_run;
+
+// import junit 4 testing framework
+import org.junit.Test;
+import org.junit.Rule;
+import static org.junit.Assert.*;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+public class AppTest {
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule();
+
+    @Test
+    public void testBar() {
+        String actual = App.bar();
+        String expected = "Hello world!";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBaz() {
+        systemOutRule.enableLog(); // start capturing System.out
+        // systemOutRule.clearLog();
+        App.baz();
+        assertEquals("Hello world!\n", systemOutRule.getLog());
+    }
+
+    @Test
+    public void testFoo() {
+        // assertion 1
+        String actual = App.foo("Hello", "world!");
+        String expected = "Hello world!";
+        assertEquals(expected, actual);
+        // assertion 2
+        actual = App.foo("World", "hello!");
+        expected = "World hello!";
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMain() {
+        systemOutRule.enableLog(); // start capturing System.out
+        String[] args = {};
+        systemOutRule.enableLog();
+        try {
+            App.main(args);
+            assertEquals("Hello world!\nHello world!\nHello world!\n", systemOutRule.getLog());
+        }
+        catch (Exception e) {
+            assert false; // fail the test if any exception occurs
+        }
+
+    }
+
+}
