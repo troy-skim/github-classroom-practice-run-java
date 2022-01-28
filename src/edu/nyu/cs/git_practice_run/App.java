cae1bef8fccbb624b26fe74db6801dacab779a5b
bloombar <amos@cs.nyu.edu>
2022-01-27 23:22:11 -0500

    positioning javadoc correctly

--- src/edu/nyu/cs/git_practice_run/App.java
+++ src/edu/nyu/cs/git_practice_run/App.java
@@ -2,42 +2,44 @@ package edu.nyu.cs.git_practice_run;
 
 public class App {
 
+  /**
+   * Concatenates two strings together and returns the result.
+   * 
+   * @param x a String
+   * @param y another String
+   * @returns The concatenation of x and y
+   */
   public static String foo(String x, String y) {
-    /**
-     * Concatenates two strings together and returns the result.
-     * 
-     * @param x a String
-     * @param y another String
-     * @returns The concatenation of x and y
-     */
 
     return x + " " + y;
   }
 
+  /**
+   * @returns The String, "Hello world!"
+   */
   public static String bar() {
-    /**
-     * @returns The String, "Hello world!"
-     */
     
     return "Hello world!";
   }
 
+  /**
+   * Prints out the String, "Hello world!"
+   */
   public static void baz() {
-    /**
-     * Prints out the String, "Hello world!"
-     */
 
     System.out.println("Hello world!");
   }
 
+  /**
+   * The main function is automatically called first in a Java program.
+   * You will complete this function.
+   * 
+   * @param args An array of any command-line arguments.
+   * @throws Exception
+   */
   public static void main(String[] args) throws Exception {
-    /**
-     * The main function is automatically called first in a Java program.
-     * You will complete this function.
-     * 
-     * @param args An array of any command-line arguments.
-     */
 
+    // complete this function according to the instructions.
 
   }
 
