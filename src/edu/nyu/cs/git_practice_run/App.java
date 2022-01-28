38ed5d081998fae99558cf3596ed08deb4cb7b00
bloombar <amos@cs.nyu.edu>
2022-01-28 11:45:13 -0500

    updated main comments

--- src/edu/nyu/cs/git_practice_run/App.java
+++ src/edu/nyu/cs/git_practice_run/App.java
@@ -34,8 +34,8 @@ public class App {
    * The main function is automatically called first in a Java program.
    * You will complete this function.
    * 
-   * @param args An array of any command-line arguments.
-   * @throws Exception
+   * @param args An array of any command-line arguments.  The main function always has to include this, even if not used.
+   * @throws Exception The main function always has to include this to handle crashing programs.
    */
   public static void main(String[] args) throws Exception {
 
